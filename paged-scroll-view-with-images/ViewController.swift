447db9319e6e9c0b6f9843a36211a5265e33d1d8
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-11-24 10:57:18 +1100

    Initial Commit

---
+++ paged-scroll-view-with-images/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  paged-scroll-view-with-images
+//
+//  Created by Evgenii Neumerzhitckii on 24/11/2014.
+//  Copyright (c) 2014 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
